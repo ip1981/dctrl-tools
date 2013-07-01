e4b8a875089a347e62b1b13866408b0a57cbabd2
Igor Pashev <pashev.igor@gmail.com>
2013-07-01 19:21:57 +0400

    Include config.h

--- sort-dctrl/sort-dctrl.c
+++ sort-dctrl/sort-dctrl.c
@@ -16,6 +16,10 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <argp.h>
 #include <locale.h>
 #include <stdlib.h>
